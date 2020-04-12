66a7a955b20e79e78061dd331159dde7581c545d
b0n3l3ss <b0n3l3ss@github.com>
2020-04-12 13:27:56 -0700

    a few functions added

--- hw1_JonasFahrney.cpp
+++ hw1_JonasFahrney.cpp
@@ -7,7 +7,8 @@ using namespace std;
 #include <regex>
 
 int returnInt(char input);
-string encrypt(string plainText, string keyWord);
+string returnChar(int input);
+string encrypt(string plainText, string keyword);
 
 int main() {
 
@@ -22,7 +23,7 @@ int main() {
 	}
 	if (userInput == "1") {
 		cout << "Enter text file name: ";
-		string output = encryptj
+		string output = ""; // finish this jonas
 	}
 
 
@@ -86,8 +87,71 @@ int returnInt(char input) {
 		return -1;
 
 }
-string encrypt(string plainText, string keyWord) {
+string returnChar(int input) {
+	if (input == 0)
+		return "a";
+	else if (input == 1)
+		return "b";
+	else if (input == 2)
+		return "c";
+	else if (input == 3)
+		return "d";
+	else if (input == 4)
+		return "e";
+	else if (input == 5)
+		return "f";
+	else if (input == 6)
+		return "g";
+	else if (input == 7)
+		return "h";
+	else if (input == 8)
+		return "i";
+	else if (input == 9)
+		return "j";
+	else if (input == 10)
+		return "k";
+	else if (input == 11)
+		return "l";
+	else if (input == 12)
+		return "m";
+	else if (input == 13)
+		return "n";
+	else if (input == 14)
+		return "o";
+	else if (input == 15)
+		return "p";
+	else if (input == 16)
+		return "q";
+	else if (input == 17)
+		return "r";
+	else if (input == 18)
+		return "s";
+	else if (input == 19)
+		return "t";
+	else if (input == 20)
+		return "u";
+	else if (input == 21)
+		return "v";
+	else if (input == 22)
+		return "w";
+	else if (input == 23)
+		return "x";
+	else if (input == 24)
+		return "y";
+	else if (input == 25)
+		return "z";
+	else
+		return "!";
+}
+string encrypt(string plainText, string keyword) {
 
-	 
+	string output = "";
+	int keywordCounter = 0;
+	for (int i = 0; i < plainText.length(); ++i) {
+		if (keywordCounter >= keyword.lentgh())
+			keywordCounter = 0;
 
+		++keywordCounter;
+	}
+	return output;
 }
